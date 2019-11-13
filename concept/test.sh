571b82ebd57d9212ec3141e9ecae23737b75810c
Ki Yung Ahn <kyagrd@gmail.com>
2019-11-13 16:31:38 +0900

    just one preprocessing

--- concept/test.sh
+++ concept/test.sh
@@ -1,2 +1,2 @@
-enscript test-in.ghci --highlight=haskell --color --language=html -o test-in-raw.html
-ghci -ghci-script test-out.ghci < /dev/null | enscript --highlight=haskell --color --language=html -o test-out-raw.html
+enscript test.ghci --highlight=haskell --color --language=html -o test-in-raw.html
+ghci -ghci-script test.ghci < /dev/null | enscript --highlight=haskell --color --language=html -o test-out-raw.html
