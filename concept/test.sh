fcc9e0f7ce3760beef5c401877ed35b644e4014f
Ki Yung Ahn <kyagrd@gmail.com>
2019-11-20 15:34:09 +0900

    test out

--- concept/test.sh
+++ concept/test.sh
@@ -2,4 +2,6 @@ runhaskell AddEcho.hs < hello.ghci > test.ghci
 enscript test.ghci --highlight=haskell --color --language=html -o hello-in-raw.html
 runhaskell TagInput.hs < hello-in-raw.html > test-in-raw.html
 
-ghci -ghci-script test.ghci < /dev/null | enscript --highlight=haskell --color --language=html -o test-out-raw.html
+ghci -ghci-script test.ghci < /dev/null \
+	| enscript --highlight=haskell --color --language=html -o hello-out-raw.html
+runhaskell TagOutput.hs < hello-out-raw.html > test-out-raw.html
