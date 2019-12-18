b9211abdd98120bad7ab896e8d6ccb8da1aca261
Ki Yung Ahn <kyagrd@gmail.com>
2019-12-18 14:39:59 +0900

    stderr redirection in test.sh

--- concept/test.sh
+++ concept/test.sh
@@ -2,6 +2,6 @@ runhaskell AddEcho.hs < hello.ghci > test.ghci
 enscript test.ghci --highlight=haskell --color --language=html -o hello-in-raw.html
 runhaskell TagInput.hs < hello-in-raw.html > test-in-raw.html
 
-ghci -ghci-script test.ghci < /dev/null \
+ghci -ghci-script test.ghci 2>&1 < /dev/null \
 	| enscript --highlight=haskell --color --language=html -o hello-out-raw.html
 runhaskell TagOutput.hs < hello-out-raw.html > test-out-raw.html
